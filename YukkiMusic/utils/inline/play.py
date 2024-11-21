5e27faac56a832e2d3d0061d426d6c01c27270a3
AtroMusic <sadeqatro916@gmail.com>
2024-11-21 14:22:23 +0430

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -301,4 +301,15 @@ def panel_markup_1(_, videoid, chat_id):
     return buttons
 
 
+def panel_markup_2(_, videoid, chat_id):
+    buttons = [
 
+    ]
+    return buttons
+
+
+def panel_markup_3(_, videoid, chat_id):
+    buttons = [
+        
+    ]
+    return buttons
